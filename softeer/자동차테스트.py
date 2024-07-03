6bd6a8aa7115f8765f078ba8a9842e927437d744
LeeSpect <wewqwew153@gmail.com>
2024-07-03 11:04:55 +0900

    .

---
+++ softeer/자동차테스트.py
@@ -0,0 +1,18 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+n, q = map(int, input().split())
+cars = list(map(int, input().split()))  
+dic = defaultdict(list)
+
+cars.sort()
+for i in range(len(cars)):
+    dic[cars[i]] = [i, len(cars)-1 - i]
+
+for _ in range(q):
+    query = int(input())
+    if dic[query]:
+        print(dic[query][0] * dic[query][1])
+    else:
+        print(0)

e73fa1af7631cfa59f93b489a1271162f6b46114
LeeSpect <wewqwew153@gmail.com>
2024-08-08 09:07:39 +0900

    .

--- Baekjoon/Gold 5/G5_12865_평범한배낭.py
+++ Baekjoon/Gold 5/G5_12865_평범한배낭.py
@@ -1,13 +1,15 @@
+# 215,196 KB / 500 ms
 import sys
 import heapq
 
-input=sys.stdin.readline
+input = sys.stdin.readline
 
 def knapsack_DP(l, n, k):
-    D=[[0] * (k+1)]
-    for i in range(1,n+1):
-        D.append([])
-        for capa in range(k+1):
+    D = [[0] * (k + 1)]
+    
+    for i in range(1, n+1):
+        D.append([])    
+        for capa in range(k + 1):
             if v == 0:
                 D[i].append(0)
             elif l[i-1][0] > capa:
@@ -17,7 +19,7 @@ def knapsack_DP(l, n, k):
     return D[-1][-1]
 
 n, k = map(int, input().split())
-l=[]
+l = []
 for i in range(n):
     w, v = map(int, input().split())
     heapq.heappush(l, (w, v))
